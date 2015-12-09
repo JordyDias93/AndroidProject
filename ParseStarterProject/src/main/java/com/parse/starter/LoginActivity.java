72c3a9f00b2a1430041e6e04d931637b1465096e
JordyDias <cappiedias@gmail.com>
2015-12-09 10:42:14 +0100

    First time
    Début du projet

---
+++ ParseStarterProject/src/main/java/com/parse/starter/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.parse.starter;
+
+import android.app.Activity;
+
+/**
+ * Created by Dr.Dias on 09/12/2015.
+ */
+public class LoginActivity extends Activity {
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.parse.starter;
+
+import android.app.Activity;
+
+/**
+ * Created by Dr.Dias on 09/12/2015.
+ */
+public class SignUpActivity extends Activity {
+
+
+}
