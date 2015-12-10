d6b2de6616fb732c55f8e0f03d5fdffc20a20259
JordyDias <cappiedias@gmail.com>
2015-12-10 20:12:02 +0100

    Ajout de ce qu'il y a faire

---
+++ ParseStarterProject/src/main/java/com/parse/starter/AFAIRE.java
@@ -0,0 +1,15 @@
+package com.parse.starter;
+
+/**
+ * Created by Dr.Dias on 10/12/2015.
+ *
+ *
+ * Login/signup
+ * créer conversation
+ * add contact
+ * recherche contact
+ * ajout photo
+ *
+ */
+public class AFAIRE {
+}
