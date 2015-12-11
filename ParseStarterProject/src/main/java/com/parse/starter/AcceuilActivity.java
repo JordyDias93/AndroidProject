a0e950ae5e2d39dddf69b428ea48aa98b839a4da
JordyDias <cappiedias@gmail.com>
2015-12-11 16:25:50 +0100

    Ajout : -Sign up (aucune correction ou validation) -log in (aucune correction)

--- ParseStarterProject/src/main/java/com/parse/starter/AcceuilActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/AcceuilActivity.java
@@ -1,10 +1,59 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
+
+import java.text.ParseException;
 
 /**
  * Created by Dr.Dias on 10/12/2015.
  */
 public class AcceuilActivity  extends Activity {
+    private static final String TAG = "Acceuil_Activity";
+    Button buttonlogin;
+    Button buttonsignup;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+        Log.v(TAG, "Welcome here ! ");
+        buttonlogin = (Button)findViewById(R.id.buttonlogin);
+        buttonsignup= (Button)findViewById(R.id.buttonsignup);
+        final EditText login = (EditText) findViewById(R.id.usernameG);
+        final EditText pass = (EditText) findViewById(R.id.passwordG);
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ParseUser.logInInBackground(login.getText().toString(), pass.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, com.parse.ParseException e) {
+                        if (user != null) {
+                            // Hooray! The user is logged in.
+                            Intent intent = new Intent(AcceuilActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Signup failed. Look at the ParseException to see what happened.
+                            Log.v(TAG,e.toString());
+                        }
+                    }
+                });
+            }
+        });
+        buttonsignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AcceuilActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
--- ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
@@ -1,11 +1,56 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.text.ParseException;
 
 /**
  * Created by Dr.Dias on 09/12/2015.
  */
 public class SignUpActivity extends Activity {
 
+    private static final String TAG = "Sign Up Activity";
+    Button SignUpReal;
+    ParseUser user;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        user = new ParseUser();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Log.v(TAG, "Welcome here ! ");
+        SignUpReal = (Button)findViewById(R.id.SignUpReal);
+        final EditText login = (EditText) findViewById(R.id.TUsername);
+        final EditText pass = (EditText) findViewById(R.id.TPassword);
+        final EditText mail = (EditText) findViewById(R.id.TMail);
+
+        SignUpReal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                user.setUsername(login.getText().toString());
+                user.setPassword(pass.getText().toString());
+                user.setEmail(mail.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(com.parse.ParseException e) {
+                        if (e == null) {
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Log.v(TAG,e.toString());
+                        }
+                    }
+                });
+            }
+        });
+    }
 
 }
